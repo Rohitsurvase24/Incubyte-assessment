510dbe8217f07c5dd80a1278df058cbe0ebb78a0
Rohitsurvase24 <88078065+Rohitsurvase24@users.noreply.github.com>
2021-12-21 17:44:46 +0530

    Update Python_script_read.py

--- Python_script_read.py
+++ Python_script_read.py
@@ -2,3 +2,13 @@ import pandas as pd
 
 customer_data = pd.read_csv(file,skiprow=1,sep=' | ')
 print(customer_data)
+
+#otherwise we can use csv library
+import csv
+
+with open('file.psv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter='|')
+    next(csv_reader, None)  # read once to skip the header once
+
+    for row in csv_reader:
+            print(row)
